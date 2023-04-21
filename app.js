270bad61603226f34a3da020fe3fba5a26fa783c
TheFuego <34629596+TheFuego@users.noreply.github.com>
2023-04-22 00:56:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ database.loadDatabase()
 const players = new Datastore('players.db')
 players.loadDatabase()
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
